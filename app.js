b2b6c81760d95120613107e9959ffb244cd50100
AsakiRain <1287059631@qq.com>
2022-01-01 14:26:35 +0800

    含泪修复2022-1-1没有补齐0的bug

--- app.js
+++ app.js
@@ -262,7 +262,7 @@ function moveTodo(ele) {
 }
 
 function getDate(date) {
-    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}T${('0' + date.getHours()).substr(-2)}:${('0' + date.getMinutes()).substr(-2)}`;
+    return `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).substr(-2)}-${('0' + date.getDate()).substr(-2)}T${('0' + date.getHours()).substr(-2)}:${('0' + date.getMinutes()).substr(-2)}`;
 }
 
 document.getElementById('btnAdd').onclick = function () {
